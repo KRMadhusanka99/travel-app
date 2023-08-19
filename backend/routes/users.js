bc26d02e3cc5253d93d8ab420b0bd1747f969979
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2023-08-20 03:25:22 +0530

    Nodejs login user Api and error fix

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -25,5 +25,52 @@ router.post("/register", async (req,res)=>{
 })
 
 //login user
+router.post("/login", async(req,res)=>{
+    try{
+        //find user
+        const user = await User.findOne({username:req.body.username});
+        if(user){
+            //validate password
+            const validPassword = await bcrypt.compare(req.body.password,user.password);
+            !validPassword && res.status(400).json({err:"username or password is incorrect"});
+            
+            //send res
+            res.status(200).json({_id:user._id, username:user.username});
+        }else{
+            res.status(400).json({err:"username or password is incorrect"}); //400-credential error
+        }
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+// router.post("/login", async (req, res) => {
+//     try {
+//         //find user
+//         const foundUser = await User.findOne({ username: req.body.username });
+
+//         if (foundUser) {
+//             //if foundUser: compare entered password to stored/foundUser password.
+//             const validPassword = await bcrypt.compare(
+//                 req.body.password,
+//                 foundUser.password
+//             );
+//             if (validPassword) {
+//                 //if both passwords match:
+//                 res.status(200).json({ username: foundUser.username });
+//             } else {
+//                 //if both passwords dont match:
+//                 res.status(400).json({ err: "Incorrect username or password" });
+//             }
+//         } else {
+//             //if !foundUser:
+//             res.status(400).json({ err: "Incorrect username or password" });
+//         }
+
+//     } catch (error) {
+//         res.status(500).json({ error, test: 'test' });
+//     }
+// });
 
 module.exports = router;
