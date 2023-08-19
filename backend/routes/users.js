029e5741beb29b1551da0ec57cf2ef213a6d1b17
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2023-08-20 01:35:24 +0530

    Nodejs register user and hash the password by bcrypt

---
+++ backend/routes/users.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const User = require("../modules/User");
+const bcrypt = require('bcrypt');
+
+//register user
+router.post("/register", async (req,res)=>{
+    try{
+        // generate new password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password,salt);
+
+        // create new user
+        const newUser = new User({
+            username:req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        // save and send the response
+        const user = await newUser.save();
+        res.status(200).json(user._id)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//login user
