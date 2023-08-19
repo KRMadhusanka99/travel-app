c7b77484d510a8776c6e841444484da1dfd2f221
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2023-08-20 03:29:37 +0530

    NodeJs check using postman error fix

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -45,32 +45,5 @@ router.post("/login", async(req,res)=>{
     }
 });
 
-// router.post("/login", async (req, res) => {
-//     try {
-//         //find user
-//         const foundUser = await User.findOne({ username: req.body.username });
-
-//         if (foundUser) {
-//             //if foundUser: compare entered password to stored/foundUser password.
-//             const validPassword = await bcrypt.compare(
-//                 req.body.password,
-//                 foundUser.password
-//             );
-//             if (validPassword) {
-//                 //if both passwords match:
-//                 res.status(200).json({ username: foundUser.username });
-//             } else {
-//                 //if both passwords dont match:
-//                 res.status(400).json({ err: "Incorrect username or password" });
-//             }
-//         } else {
-//             //if !foundUser:
-//             res.status(400).json({ err: "Incorrect username or password" });
-//         }
-
-//     } catch (error) {
-//         res.status(500).json({ error, test: 'test' });
-//     }
-// });
 
 module.exports = router;
