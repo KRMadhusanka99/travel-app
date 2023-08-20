a94d7de6371a6bced3004a8444cad82e1a9d6ad7
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2023-08-20 14:14:46 +0530

    add Mapbox APIs to React fronntend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-
+import * as React from "react";
+import ReactMapGL from 'react-map-gl';
 function App() {
+  
+  const mapboxAccessToken = process.env.REACT_APP_MAPBOX;
+  
   return (
-    <div className="App">
-      Hello
-    </div>
+      <ReactMapGL
+        mapboxAccessToken={mapboxAccessToken}
+        initialViewState={{
+          longitude: 80.220978, //79.861244,
+          latitude:  6.053519, //6.927079,
+          zoom:10
+        }}  
+        style={{width: "100vw", height: "100vh"}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
   );
 }
 
