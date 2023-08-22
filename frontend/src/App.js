0e8a1054efd3610d6a5bc9f64046b2a1939a7fb7
KRMadhusanka99 <kkrmadhu1999@gmail.com>
2023-08-22 22:19:36 +0530

    add gitignore file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,10 +8,11 @@ import {format} from 'timeago.js';
 import { useState } from "react";
 
 function App() {
-  const currentUser = 'Ramesh';
+  const currentUser = 'Rameh';
   const mapboxAccessToken = process.env.REACT_APP_MAPBOX;
   const [pins,setPins] = useState([]);
   const [currentPlaceId, setCurrentPlaceId] = useState(null);
+  //const [newPlace, setNewPlace] = useState(null);
   
   React.useEffect(()=>{
     const getPins = async () => {
@@ -27,6 +28,11 @@ function App() {
 
   const handleMarkerClick = (id) => {
     setCurrentPlaceId(id);
+    console.log(id)
+  };
+
+  const handleAddClick = (e) =>{
+    console.log(e);
   }
 
   return (
@@ -36,9 +42,10 @@ function App() {
         initialViewState={{
           longitude: 80.220978, //79.861244,
           latitude:  6.053519, //6.927079,
-          zoom:8
+          zoom:8,
+          
         }}  
-        style={{width: "100vw", height: "100vh"}}
+        style={{width: "100vw", height: "100vh",ondblclick:{handleAddClick}}}
         mapStyle="mapbox://styles/kkrmadhu1999/clllb5g5n01eb01plhhxdbv3z"
         // "mapbox://styles/kkrmadhu1999/clll5pig6005201o1g6ky7e75"
       >
@@ -51,7 +58,7 @@ function App() {
              ></RoomIcon>
            </Marker>
            
-           {p._id === currentPlaceId ||
+           {p._id === currentPlaceId || 
            <Popup longitude={p.long} latitude={p.lat}
              anchor="bottom-left" style={{maxWidth: '10px', fontSize: '14px', color:'black', cursor:'pointer'}}
              onClose={()=>setCurrentPlaceId(null)}
@@ -74,28 +81,25 @@ function App() {
            }
            </>
         ))}
-
-         {/* <Marker longitude={80.216797} latitude={6.028624} anchor="bottom"
-         >
-            <RoomIcon style={{ color: 'red', fontSize: '20px'}}></RoomIcon>
-          </Marker>
-          <Popup longitude={80.220978} latitude={6.053519}
-            anchor="bottom-left" style={{maxWidth: '10px', fontSize: '14px', color:'black'}}>
-              <div className="text">
-                <label>Place</label><br/>
-                <h4 className="place">Galle Fort</h4><br/>
-                <label>Review</label><br/>
-                <p className="des">Very beautiful place. I like it </p><br/>
-                <label>Rating</label><br/>
-                <div className="stars">
-                5
-                <StarIcon className="star"/>
-                </div><br/>
-                <label>Information</label><br/>
-                <span className="username">Created by -<b>  Ramesh</b></span><br/>
-                <span className="date">1 hour ago</span>
-              </div>
-          </Popup> */}
+        {/* <Popup longitude={} latitude={}
+             anchor="bottom-left" style={{maxWidth: '10px', fontSize: '14px', color:'black', cursor:'pointer'}}
+             onClose={()=>setCurrentPlaceId(null)}
+             >
+               <div className="text">
+                 <label>Place</label><br/>
+                 <h4 className="place">{p.title}</h4><br/>
+                 <label>Review</label><br/>
+                 <p className="des">{p.desc}</p><br/>
+                 <label>Rating</label><br/>
+                 <div className="stars">
+                 5
+                 <StarIcon className="star"/>
+                 </div><br/>
+                 <label>Information</label><br/>
+                 <span className="username">Created by -<b>{p.username}</b></span><br/>
+                 <span className="date">{format(p.createdAt)}</span>
+               </div>hello
+           </Popup> */}
 
       </ReactMapGL>
       </div>
